2a674e71d4e9cb999663b4600494c112648a54fb
TitanHZZ <titanhzz2018@gmail.com>
2025-05-27 10:47:19 +0100

    More scripts.

---
+++ conn_timings.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import ipaddress
+import dns.resolver
+import dns.reversename
+import geoip2.database
+import matplotlib.pyplot as plt 
+
+datafile = 'dataset6/data6.parquet'
+
+### Read parquet data files
+data = pd.read_parquet(datafile)
+
+data['diff_timestamp'] = data.groupby(['src_ip'])['timestamp'].diff().fillna(0)
+timestamp = data.groupby(['src_ip'])['diff_timestamp'].mean().sort_values(ascending=False)
+mean = timestamp.mean()
+std = timestamp.std()
+print("mean timestamp:", mean)
+print("std timestamp:", std)
+
+# TODO:
+# this gives us a 'mean' and 'std' that do not cover the entire range for the 'good' actors
+# meaning that we would have false positives (is this accepted?)
+# print(timestamp)
+
+datafile = 'dataset6/servers6.parquet'
+
+### Read parquet data files
+data = pd.read_parquet(datafile)
+
+data['diff_timestamp'] = data.groupby(['src_ip'])['timestamp'].diff().fillna(0)
+timestamp = data.groupby(['src_ip'])['diff_timestamp'].mean().sort_values(ascending=False)
+print("mean timestamp:", (timestamp).mean())
+print("std timestamp:", (timestamp).std())
+
+weird_timings = data.loc[(data["diff_timestamp"] > (mean + std)) | (data["diff_timestamp"] < (mean - std))]
+print(weird_timings)
--- http_dns_exfiltration.py
+++ http_dns_exfiltration.py
@@ -5,20 +5,20 @@ datafile = 'dataset6/data6.parquet'
 ### Read parquet data files
 data = pd.read_parquet(datafile)
 
-#Number of connections
+# Number of connections
 port443 = data.loc[data['port'] == 443 ].groupby(['src_ip']).count()["up_bytes"]
 port53 = data.loc[data['port'] == 53].groupby(['src_ip']).count()["down_bytes"]
 
-#Ratio of connections
-ratio = pd.DataFrame(port443/ port53, columns=['ratio'])
+# Ratio of connections
+ratio = pd.DataFrame(port443 / port53, columns=['ratio'])
 up_down_ratio = pd.concat([port443, port53, ratio], axis=1)
 
-mean = (port443/ port53).mean()
-std = (port443/ port53).std()
+mean = (port443 / port53).mean()
+std = (port443 / port53).std()
 
 print(up_down_ratio)
-print("Mean: ",mean)
-print("Std: ",std)
+print("Mean: ", mean)
+print("Std: ", std)
 
 datafile = 'dataset6/test6.parquet'
 
@@ -35,6 +35,6 @@ print(up_down_ratio)
 print("Mean: ",(port443/ port53).mean())
 print("Std: ",(port443/ port53).std())
 
-#TODO: Probably adjust the value mean-std
+# TODO: Probably adjust the value mean-std
 dns_exfiltration = up_down_ratio.loc[up_down_ratio["ratio"] < (mean-(std*2))]
 print(dns_exfiltration)
